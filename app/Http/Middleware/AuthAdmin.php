9e8bc0b029c498dac3d5311b8239ddc2212391c9
liucg <164445438@qq.com>
2020-12-17 16:03:55 +0800

    中间件

---
+++ app/Http/Middleware/AuthAdmin.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Providers\RouteServiceProvider;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (Auth::check()) {
+            return $next($request);
+        } else {
+            return redirect(route('admin.login'));
+        }
+    }
+}
